f527f20f0fca23fe4288c2e95d18aa2123c059b2
AndreaGold99 <andreainazuma@gmail.com>
2023-02-04 15:52:10 +0100

    point
    esta vacia

---
+++ Botica/Assets/Scripts/Point.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour
+{
+    public List<Item> itemsToDo;
+    public Transform[] pointsToDrop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
